559bf8d4d0c08baf2f8fc233312c53aeb231f4a5
AlbertRipak <arspak107@gmail.com>
2024-05-10 14:50:34 +0300

    modify check_size, added the ability to send output in one message

--- procedure-check_size_file.sql
+++ procedure-check_size_file.sql
@@ -1,15 +1,15 @@
 DECLARE
-    v_table_name        dba_data_files.tablespace_name%TYPE;
-    v_size_file              dba_data_files.bytes%TYPE;
-    v_used_bytes        dba_extents.bytes%TYPE;
-    v_free_bytes          dba_free_space.bytes%TYPE;
-    v_procent_used     dba_extents.bytes%TYPE;
-    msg_body               VARCHAR2(1000);
+    v_table_name            dba_data_files.tablespace_name%TYPE;
+    v_size_file             dba_data_files.bytes%TYPE;
+    v_used_bytes            dba_extents.bytes%TYPE;
+    v_free_bytes            dba_free_space.bytes%TYPE;
+    v_procent_used          dba_extents.bytes%TYPE;
+    msg_body                VARCHAR2(1000);
     CURSOR c_file_size IS
-        SELECT dba_df.tablespace_name                                                  AS   "Tablespace Name",
-        SUM(dba_df.bytes)/1024/1024                                                         AS   "Size (MB)",
-        NVL(SUM(Used.used_bytes)/1024/1024,0)                                    AS   "Used (MB)",
-        NVL(SUM(Free.free_bytes)/1024/1024,0)                                       AS   "Free (MB)",
+        SELECT dba_df.tablespace_name                                  AS   "Tablespace Name",
+        SUM(dba_df.bytes)/1024/1024                                    AS   "Size (MB)",
+        NVL(SUM(Used.used_bytes)/1024/1024,0)                          AS   "Used (MB)",
+        NVL(SUM(Free.free_bytes)/1024/1024,0)                          AS   "Free (MB)",
         NVL((SUM(Used.used_bytes) * 100) / SUM(dba_df.bytes),0)        AS    "Used %"
     FROM dba_data_files dba_df,
         (SELECT file_id, SUM(nvl(bytes,0)) used_bytes 
@@ -28,18 +28,13 @@ BEGIN
             FETCH c_file_size
             INTO v_table_name, v_size_file, v_used_bytes, v_free_bytes, v_procent_used;
         EXIT WHEN c_file_size%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE('Tablespace Name = ' || v_table_name ||
+        msg_body := msg_body || ('Tablespace Name = ' || v_table_name ||
                     ', Size (MB) = ' || v_size_file || 
                     ', Used (MB) = ' || v_used_bytes ||
                     ', Free (MB) = ' || v_free_bytes || 
-                    ', Used % = ' || v_procent_used );
-        msg_body := (v_size_file || v_used_bytes || v_free_bytes || v_procent_used);
-            IF v_procent_used > 80 THEN 
-                send_mail('my@post.test.com', ' Please check the size of the DBF files!', msg_body);
-            ELSE 
-                send_mail('my@post.test.com', ' Please check configuration!', msg_body);
-            END IF;
+                    ', Used % = ' || v_procent_used ); 
         END LOOP;
+        send_mail('test@post.test.ua', 'Something text', msg_body);
     CLOSE c_file_size;
 END;
 /
--- procedure-send_mail.sql
+++ procedure-send_mail.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE PROCEDURE send_mail (
-    msg_to VARCHAR2,
-    msg_text VARCHAR2,
-    msg_body VARCHAR2)
+    msg_to      VARCHAR2,
+    msg_text    VARCHAR2,
+    msg_body    VARCHAR2)
 IS
     c UTL_SMTP.connection;
-    msg_from VARCHAR2(50) := '192.168.1.36'; -- ip address database server 
+    msg_from VARCHAR2(50) := '192.168.1.194'; -- ip address database server 
     msg_subject VARCHAR2(100) := 'Oracle Database (DBUP).'; 
     mailhost    VARCHAR2(30) := 'post.test.com'; --domain address or ip address for post server
     mailport    INTEGER := 8080;
     base64username VARCHAR2(30) := UTL_RAW.cast_to_varchar2(UTL_ENCODE.base64_encode(UTL_RAW.cast_to_raw('alerto.mail')));
-    base64password VARCHAR2(30) := UTL_RAW.cast_to_varchar2(UTL_ENCODE.base64_encode(UTL_RAW.cast_to_raw('my_post_password')));
+    base64password VARCHAR2(30) := UTL_RAW.cast_to_varchar2(UTL_ENCODE.base64_encode(UTL_RAW.cast_to_raw('sldkjfldsjf435#$#@%$#')));
 BEGIN
     c  := UTL_SMTP.open_connection(mailhost, mailport);
     UTL_SMTP.helo(c, mailhost);
@@ -23,7 +23,7 @@ BEGIN
     UTL_SMTP.write_data(c, 'From: Oracle Database DBUP' || UTL_TCP.crlf);
     UTL_SMTP.write_data(c, 'To: ' || msg_to || UTL_TCP.crlf);
     UTL_SMTP.write_data(c, 'Subject: ' || msg_subject || UTL_TCP.crlf);
-    
+
     UTL_SMTP.write_data(c, msg_text  || UTL_TCP.crlf);
     UTL_SMTP.write_data(c, UTL_TCP.crlf || msg_body);
     UTL_SMTP.write_data(c, UTL_TCP.crlf || UTL_TCP.crlf);
